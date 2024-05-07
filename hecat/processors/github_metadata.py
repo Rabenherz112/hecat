08c64ede3b3899d00757806af31559df265da8ce
Rabenherz112 <rabenherz@theravenhub.com>
2024-05-07 21:07:24 +0200

    Fixed wrong affiliation in the metadata to files
    These changes have been tested by running a full metadata processing on the awesome-selfhosted-data repository and checking the metadata files for the correct affiliation.
    
    Bug Fixes:
    - Metadata is now not being assigned via a index but instead by matching the `url` field in the return of GraphQL query to the `source_code_url`
    
    Logging:
    - Added more information about the status of the metadata processing (as this can now take a while to process)
    - Added more debug information for Ratelimit Information from GitHub API
    
    Defaults:
    - Added a default wait-time between API requests to GitHub to avoid hitting the rate limit (default is now 60 seconds, can be configured in the `hecat.yml` file)
    - Added a default batch-size for the metadata processing (default is now 30, can be configured in the `hecat.yml` file)
    
    Others:
    - Added new function `extract_repo_name` to extract the repo name from the `source_code_url`
    - Added try-catch block to catch exceptions when the writing metadata to a file
    - Updated documentation to reflect new batch_size configuration option and new API restrictions from GitHub
    
    Co-authored-by: Le Duc Lischetzke <lischetzke@users.noreply.github.com>

--- hecat/processors/github_metadata.py
+++ hecat/processors/github_metadata.py
@@ -8,7 +8,8 @@ steps:
     module_options:
       source_directory: tests/awesome-selfhosted-data # directory containing YAML data and software subdirectory
       gh_metadata_only_missing: False # (default False) only gather metadata for software entries in which one of stargazers_count,updated_at, archived is missing
-      sleep_time: 3.7 # (default 0) sleep for this amount of time before each request to Github API
+      sleep_time: 3.7 # (default 60) sleep for this amount of time before each request to Github API
+      batch_size: 10 # (default 30) number of repositories to include in each batch request to Github API
 
 source_directory: path to directory where data files reside. Directory structure:
 ├── software
@@ -26,8 +27,8 @@ On Github Actions a token is created automatically for each job. To make it avai
 env:
   GITHUB_TOKEN: ${{secrets.GITHUB_TOKEN}}
 
-When using GITHUB_TOKEN, the API rate limit is 1,000 requests per hour per repository [[1]](https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#rate-limits-for-requests-from-github-actions)
-Not that each call to get_gh_metadata() results in 2 API requests (on for the repo/stargazers count, one for the latest commit date)
+When using GITHUB_TOKEN, the API rate limit is 1,000 requests per hour per repository [[1]](https://docs.github.com/en/graphql/overview/rate-limits-and-node-limits-for-the-graphql-api#primary-rate-limit)
+The call to get_gh_metadata() performs an API request for every batch with `batch_size` repositories (which returns all metadata in one request).
 """
 
 import sys
@@ -39,7 +40,6 @@ import os
 import time
 from datetime import datetime
 import ruamel.yaml
-import github
 from ..utils import load_yaml_data, to_kebab_case
 
 yaml = ruamel.yaml.YAML(typ='rt')
@@ -66,6 +66,10 @@ def write_software_yaml(step, software):
     logging.debug('writing file %s', dest_file)
     with open(dest_file, 'w+', encoding="utf-8") as yaml_file:
         yaml.dump(software, yaml_file)
+
+def extract_repo_name(url):
+    re_result = re.search(r'^https?:\/\/github\.com\/[^\/]+\/([^\/]+)\/?$', url)
+    return re_result.group(1) if re_result else None
 
 def add_github_metadata(step):
     """gather github project data and add it to source YAML files"""
@@ -107,71 +111,106 @@ def add_github_metadata(step):
     # Get the URLs of the queued repositories
     github_urls = [software['source_code_url'] for software in github_projects]
     repos = [re.sub('https://github.com/', '', url) for url in github_urls]
-    projectindex = 0
 
-    # Split the list of repositories into batches of 60
-    # TODO: While more should be supported, I don't get it to work with 75 or more, as the API returns an error
-    n = 60
-    batches = [repos[i * n:(i + 1) * n] for i in range((len(repos) + n - 1) // n )]
+    # TODO: While more should be supported, I don't get it to work with 50 or more, as the API returns an error
+    # This limit was tested with a personal access token, batch_size = 30, timeout = 60 (new default if not provided in a config); Worked fine for the full repo (enable debug messages to see usage of API Rate limit stats)
+    # Split repo list into batches of batch_size
+    if 'batch_size' in step['module_options']:
+        batch_size = step['module_options']['batch_size']
+    else:
+        # Default batch_size of repos if not specified in config file
+        batch_size = 30
+    batches = [repos[i * batch_size:(i + 1) * batch_size] for i in range((len(repos) + batch_size - 1) // batch_size )]
 
-
+    counter = 0
     for batch in batches:
+        counter += 1
+        logging.info(f"Processing batch {counter}/{len(batches)}")
+        
         repos_query = " ".join([f"repo:{repo}" for repo in batch])
 
         # Get the current year and month
         now = datetime.now()
         year_month = now.strftime("%Y-%m")
+        # TODO: More accurate lookup would be for last month, but would mean we lag 1 month behind
+        #dateMonth = (now.month - 2) % 12 + 1
+        #dateYear = now.year - 1 if dateMonth == 12 else now.year
+        #year_last_month = datetime.date(dateYear, dateMonth, 1).strftime("%Y-%m")
 
         query = f"""
-        {{
-          search(
-            type: REPOSITORY
-            query: "{repos_query}"
-            first: 60
-          ) {{
-            repos: edges {{
-              repo: node {{
-                ... on Repository {{
-                  name
-                  stargazerCount
-                  isArchived
-                  releases(first: 1) {{
-                    edges {{
-                      node {{
+{{
+    search(
+        type: REPOSITORY
+        query: "{repos_query}"
+        first: {batch_size}
+    ) {{
+    repos: edges {{
+        repo: node {{
+        ... on Repository {{
+            url
+            stargazerCount
+            isArchived
+            releases(first: 1) {{
+                edges {{
+                    node {{
                         tagName
                         publishedAt
-                      }}
                     }}
-                  }}
-                  defaultBranchRef {{
-                    target {{
-                      ... on Commit {{
+                }}
+            }}
+            defaultBranchRef {{
+                target {{
+                    ... on Commit {{
                         committedDate
                         history(since: "{year_month}-01T00:00:00", until: "{year_month}-31T23:59:59") {{
-                          totalCount
+                            totalCount
                         }}
-                      }}
                     }}
-                  }}
                 }}
-              }}
             }}
-          }}
         }}
+        }}
+    }}
+    }}
+}}
         """
+        res_header = None
         try:
             response = requests.post(GITHUB_GRAPHQL_API, json={"query": query}, headers=headers)
+            res_header = response.headers
+            # Check status code
+            if response.status_code != 200:
+                # print body
+                errors.append(f'Response code of POST request (GraphQL): {response.status_code}')
             data = response.json()
             if 'errors' in data:
                 for error in data['errors']:
                     errors.append(error['message'])
-                sys.exit(1)
+                sys.exit(4)
         except Exception as e:
             errors.append(str(e))
-
+            
+        # casefold header names
+        if res_header:
+            res_header = {k.casefold(): v for k, v in res_header.items()}
+            rl_arr = []
+            rl_arr.append(res_header['x-ratelimit-limit']) if 'x-ratelimit-limit' in res_header else rl_arr.append('-1')
+            rl_arr.append(res_header['x-ratelimit-remaining']) if 'x-ratelimit-remaining' in res_header else rl_arr.append('-1')
+            rl_arr.append(res_header['x-ratelimit-used']) if 'x-ratelimit-used' in res_header else rl_arr.append('-1')
+            rl_arr.append(res_header['x-ratelimit-reset']) if 'x-ratelimit-reset' in res_header else rl_arr.append('-1')
+            logging.debug(f"Rate limit (Limit/Remain/Used/Reset): {'/'.join(rl_arr)}")
+            
         for edge in data["data"]["search"]["repos"]:
             repo = edge["repo"]
-            software = github_projects[projectindex]
+            software = None
+            for project in github_projects:
+                if extract_repo_name(repo["url"]).casefold() == extract_repo_name(project['source_code_url']).casefold():
+                    software = project
+                    break
+            if not software:
+                logging.error('could not find software entry for %s', repo["url"])
+                continue
+            
             software["stargazers_count"] = repo["stargazerCount"]
             software["archived"] = repo["isArchived"]
             if repo["releases"]["edges"] and len(repo["releases"]["edges"]) > 0:
@@ -188,17 +227,24 @@ def add_github_metadata(step):
                 software["commit_history"].update({
                     year_month: repo["defaultBranchRef"]["target"]["history"]["totalCount"]
                 })
-            projectindex += 1
-            write_software_yaml(step, software)
+            try:
+                write_software_yaml(step, software)
+            except Exception as e:
+                errors.append(str(e))
+                logging.error('could not write software entry for %s', repo["url"])
+                continue
 
         # Sleep for the specified amount of time before the next request
         if 'sleep_time' in step['module_options']:
             time.sleep(step['module_options']['sleep_time'])
+        else:
+            # Default time between Github GraphQL API requests if not specified in config file
+            time.sleep(60)
     
     if errors:
         logging.error("There were errors during processing")
         print('\n'.join(errors))
-        sys.exit(1)
+        sys.exit(2)
 
 def gh_metadata_cleanup(step):
     """remove github metadata from source YAML files"""
